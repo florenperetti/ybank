d3766ac39535b59923c936f091a99a16a6f1af46
Florencia Peretti <florenperetti@gmail.com>
2020-03-26 14:06:03 -0300

    Controllers cleanup - removed unused methods

--- api/app/Http/Controllers/AccountsController.php
+++ api/app/Http/Controllers/AccountsController.php
@@ -9,27 +9,6 @@ use App\Account;
 class AccountsController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  Account  $account
@@ -38,28 +17,5 @@ class AccountsController extends Controller
     public function show(Account $account)
     {
         return response()->json([$account]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- api/app/Http/Controllers/TransactionsController.php
+++ api/app/Http/Controllers/TransactionsController.php
@@ -46,38 +46,4 @@ class TransactionsController extends Controller
 
         return response()->json($newTransaction->load(['accountFrom', 'accountTo']));
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
